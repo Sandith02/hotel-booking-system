8cac3bbc3ecd45dd9a6b141ec8a8d365e89f4436
Sandith02 <lhthenuwara@gmail.com>
2025-05-18 18:49:39 +0530

    chore: update .env and .gitignore files; simplify MongoDB connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,9 @@
+// In your config/db.js file
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
